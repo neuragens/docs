e0250f31149bdd040973db9fa77ed5d17fe8e649
Michael Douglas Barbosa Araujo <michaeldouglas010790@gmail.com>
2024-10-12 19:36:04 -0300

    First commit

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
